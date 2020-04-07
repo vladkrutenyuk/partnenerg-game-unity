f63530654fd6a1ff758b2565498c6c00b8459b9f
KVY <kvy@phygitalism.com>
2020-04-07 08:18:05 +0300

    PE-6 camera controller refactoring

--- Assets/PARTENERG/Scripts/CameraController.cs
+++ Assets/PARTENERG/Scripts/CameraController.cs
@@ -3,8 +3,12 @@ using DG.Tweening;
 
 public class CameraController : MonoBehaviour
 {
+    private const float FallShakeDuration = 0.1f;
+    private const float FallShakeStrength = 0.35f;
+    private const int FallShakeVibrato = 10;
+
     [SerializeField] private PlayerController playerController;
-    
+
     private float _gravityFactor;
     private bool _wasGrounded;
     
@@ -13,14 +17,15 @@ public class CameraController : MonoBehaviour
         if(!_wasGrounded && playerController.isGrounded && _gravityFactor > 5)
         {
             _gravityFactor *= _gravityFactor;
-            CallCameraShakes(0.1f, new Vector3(0, 0.003f, 0) * _gravityFactor, 10, 0);
+            Vector3 strength = new Vector3(0, FallShakeStrength, 0) * _gravityFactor / 100f;
+            CallCameraShakes(FallShakeDuration, strength, FallShakeVibrato, 1);
         }
 
         _wasGrounded = playerController.isGrounded;
         _gravityFactor = -playerController.gravityComponent;
     }
 
-    private void CallCameraShakes(float duration, Vector3 strength, int vibrato, float randomness)
+    public void CallCameraShakes(float duration, Vector3 strength, int vibrato, float randomness)
     {
         playerController.mainCamera.DOShakePosition(duration, strength, vibrato, randomness, true);
         print(strength.y);
