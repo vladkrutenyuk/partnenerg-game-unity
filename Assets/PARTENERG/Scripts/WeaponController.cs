2237a6e9ef83b1f9570d9389e330a9b6640f99a2
KVY <kvy@phygitalism.com>
2020-04-09 12:58:48 +0300

    PE-7 work on weapon anim logic

---
+++ Assets/PARTENERG/Scripts/WeaponController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+    void Shoot()
+    {
+
+    }
+
+    void Reload()
+    {
+
+    }
+
+    void Switch()
+    {
+
+    }
+}
