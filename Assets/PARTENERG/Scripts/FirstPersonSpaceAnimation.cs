017154ddb43d50b27a74b490fb75be3efd05b4b4
KVY <kvy@phygitalism.com>
2020-04-12 10:20:31 +0300

    PE-9 FirstPersonSpaceAnimation refactoring

--- Assets/PARTENERG/Scripts/FirstPersonSpaceAnimation.cs
+++ Assets/PARTENERG/Scripts/FirstPersonSpaceAnimation.cs
@@ -1,38 +1,36 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using DG.Tweening;
+﻿using UnityEngine;
 
 public class FirstPersonSpaceAnimation : MonoBehaviour
 {
-    // Holder rotation when mouse rotation
+    // TO-DO Switch weapon animation
 
-    // Switch weapon animation
+    private const float RotationRemapInput = 5f;
+    private const float RotationLerpTimeMult = 3f;
+    private const string RotationXAnimatorName = "Space_RotationX";
+    private const string RotationYAnimatorName = "Space_RotationY";
+
+    private Vector2 _animatorRotation;
 
     [SerializeField] private Animator animator;
 
-    private float _mouseX;
-    private float _mouseY;
-
-    private float _rotationX;
-    private float _rotationY;
-
     private void Update() 
     {
-        _mouseX = Input.GetAxis("Mouse X");
-        _mouseY = Input.GetAxis("Mouse Y");
+        _animatorRotation.x = Mathf.Lerp(
+            _animatorRotation.x, GetAnimatorRotation(Input.GetAxis("Mouse X")), Time.deltaTime * RotationLerpTimeMult);
 
-        _mouseX = Map(_mouseX, -5, 5, -1, 1);
-        _mouseY = Map(_mouseY, -5, 5, -1, 1);
+        _animatorRotation.y = Mathf.Lerp(
+            _animatorRotation.y, GetAnimatorRotation(Input.GetAxis("Mouse Y")), Time.deltaTime * RotationLerpTimeMult);
 
-        _rotationX = Mathf.Lerp(_rotationX, _mouseX, Time.deltaTime * 3f);
-		_rotationY = Mathf.Lerp(_rotationY, _mouseY, Time.deltaTime * 3f);
+        animator.SetFloat(RotationXAnimatorName, _animatorRotation.x);
+        animator.SetFloat(RotationYAnimatorName, _animatorRotation.y);
+    }
 
-        animator.SetFloat("Space_RotationX", _rotationX);
-        animator.SetFloat("Space_RotationY", _rotationY);
+    private float GetAnimatorRotation(float input)
+    {
+        return Remap(input, -RotationRemapInput, RotationRemapInput, -1, 1);
     }
     
-    private float Map (float value, float from1, float to1, float from2, float to2) 
+    private float Remap(float value, float from1, float to1, float from2, float to2) 
     {
         return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
     }
